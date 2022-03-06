9686d03670f32413b07dfe8ca769b4237749ddbe
SaqibSiddiqi <lookfright@gmail.com>
2022-03-06 06:04:39 +0400

    login and registration pages

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //server.js Saqib Hasan Siddiqi / 301237796 / 5th febuary 2022
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let createError = require('http-errors');
+let path = require('path')
+let cookieParser = require('cookie-parser'); 
+let logger = require('morgan');
 //auth
 let session = require('express-session');
 let passport = require('passport');
@@ -24,6 +28,16 @@ app.use(flash);
 //init passport
 app.use(passport.initialize());
 app.use(passport.session());
+
+let userModel = require('./src/model/user');
+let User = userModel.User;
+
+passport.use(User.createStrategy());
+
+//serializer and Deserializer
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.set('view engine', 'ejs');
 
@@ -50,8 +64,90 @@ app.get('/contact', function(req, res) {
 app.get('/bussiness-contact', function(req, res){
 
 });
-app.get('/login', function(req, res){
 
+
+//Login Functions
+app.get('/login', function(req, res){
+    if(!req.user){
+        res.render('auth/login',{
+            title:"Login", messages :req.flash('loginMessege'), displayName : req.user? req.user.displayName:""
+        });
+    }else{
+        return res.redirect('/');
+        
+    }
+});
+app.post('/login', function(req, res){
+    passport.authenticate('local', (err, usr, info) =>{
+        if(err){
+            return next(err);
+        }
+        if(!user){
+            req.flash('loginMessage', 'Authentication Error');
+            return res.redirect('/login');
+        }
+        req.login(user, (err)=>{
+            if(err){
+                return next(err);
+            }
+            const payload = {
+                id: user._id,
+                displayName : user.displayName,
+                username : user.username,
+                email : user.email
+            }
+            return res.redirect('/business-contact');
+        });
+    });
+});
+
+
+//Register Functions
+app.get('/register', function(req, res){
+    if(!req.user){
+        res.render('auth/register',{
+            title:"Register", 
+            messages :req.flash('registerMessege'), 
+            displayName : req.user? req.user.displayName:""
+        });
+    }else{
+        return res.redirect('/');
+    }
+});
+
+app.post('/register', function(req, res){
+
+    let newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
+
+    User.register(newUser, req.body.password, (err)=>{
+        if(err){
+            console.log("Error registering new user");
+            if(err.name = "UserExistsError"){
+                req.flash(
+                    'regiserMessege', 'Registration Error: User Already Exists'
+                );
+                console.log('Error: User already Exists');
+            }
+            return res.render('auth/register', {
+                title: 'Register',
+                messages: req.flash('registerMessage'),
+                displayName: req.user? req.user.displayName: ""
+            });
+        }else{
+            return passport.authenticate('local')(req, res, ()=>{
+                res.redirect('/bussiness-contact');
+            })
+        }
+    });
+});
+
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
 });
 
 
